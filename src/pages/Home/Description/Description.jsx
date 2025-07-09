9d7834bf6cd45947f460865e618217c926259e05
Mmsshakil <softhandshakil@gmail.com>
2025-07-10 02:24:37 +0600

    description and specification added

---
+++ src/pages/Home/Description/Description.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const Description = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const fullText = `Just as a book is judged by its cover, the first thing you notice when you pick up a modern smartphone is the display. Nothing surprising, because advanced technologies allow you to practically level the display frames and cutouts for the front camera and speaker, leaving no room for bold design solutions. And how good that in such realities Apple everything is fine with displays.`;
+  const firstFadedtext = `Technologies allow you to practically level the display frames and cutouts for the front camera and speaker, leaving no room for bold design solutions. And how good that in such realities Apple everything`;
+
+  const fadedText = `Advanced technologies allow you to practically level the display frames and cutouts for the front camera and speaker, leaving no room for bold design solutions. And how good that in such realities Apple everything`;
+
+  return (
+    <div className="max-w-full mx-auto bg-white p-1 md:p-6">
+      <h2 className="text-2xl font-semibold text-[#1C1C2A] mb-4">
+        Description
+      </h2>
+
+      <p className="text-[#434A59] text-lg leading-relaxed">{fullText}</p>
+
+      {showMore ? (
+        <p className="text-[#434A59] text-lg mt-3 leading-relaxed">
+          {firstFadedtext}
+        </p>
+      ) : (
+        <p className="text-[#B0B7C3] text-lg mt-3 leading-relaxed">
+          {firstFadedtext}
+        </p>
+      )}
+
+      {showMore && (
+        <p className="text-[#434A59] text-lg mt-3 leading-relaxed">
+          {fadedText}
+        </p>
+      )}
+
+      <button
+        onClick={() => setShowMore(!showMore)}
+        className="mt-6 text-lg font-semibold text-[#1C1C2A] flex items-center  gap-1"
+      >
+        See
+        {showMore ? <p>Less</p> : <p>More</p>}
+        <span
+          className={`transform transition-transform ${
+            showMore ? "rotate-180" : ""
+          }`}
+        >
+          ⌄
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default Description;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Description from "./Description/Description";
+import Specification from "./Specification/Specification";
 
 const Home = () => {
   return (
-    <div className="bg-white text-black">
-      <h1>home</h1>
+    <div className="bg-[#F1F5F9] text-black">
+      <h1 className="bg-white">home</h1>
+
+      <div className="  max-w-7xl mx-auto grid grid-cols-3  py-1 md:py-7">
+        <div className="col-span-2 flex flex-col gap-3">
+          <div className="">
+            <Description></Description>
+          </div>
+          <div>
+            <Specification></Specification>
+          </div>
+        </div>
+        <div className="col-span-1">{/* <Description></Description> */}</div>
+      </div>
     </div>
   );
 };
